fa016b5604879031d910068430e54a67c82d0965
CarlosfcPinheiro <carlosfcpinheiro82@gmail.com>
2024-06-09 16:07:27 -0300

    atualizar virtual pet com poo

--- Exercises/poo/classes_alura/virtual_pet.py
+++ Exercises/poo/classes_alura/virtual_pet.py
@@ -1,39 +1,46 @@
-class VirtualPet():
-    def __init__(self, name, hungry=20, health=50, age=0):
+class VirtualPet:
+    def __init__(self, name: str, hungry=100, health=100, age=0):
         self.name = name
         self.hungry = hungry
         self.health = health
         self.age = age
 
-    def calcHumor(self):
-        humor = self.hungry + self.health
-        return humor
-
     def feed(self):
-        def checkHealthtoAdd(): # function to check health and then add it
-            if (self.health < 95):
+        def checkHealth():
+            if self.health <= 97:
                 self.health += 3
-            elif (self.heath+3 > 100):
-                self.heath += 100 - self.health
-
-        if (self.hungry <= 95):
-            self.hungry += 5
-            self.health += 3
-            if self.hungry == 100:
-                print(f"You feed your pet :D")
-                print(f"Your actual hungry is 100!")
             else:
-                print(f"You feed your pet :D")
-                print(f"Actual hungry: {self.hungry}")
-        
-        elif (self.hungry+5 > 100):
+                self.heatl += 100 - self.health
+
+        if (self.hungry < 95):
+            self.hungry += 5
+            checkHealth()
+        else:
             self.hungry += 100 - self.hungry
-            checkHealthtoAdd()  
-            print("You feed your pet :D")
-            print(f"You actual hungry is 100!")
+            checkHealth()
 
     def showStatus(self):
-        print(f"""{self.name}'s Satus:
-            Hungry: {self.hungry}
-            Health: {self.health}
-            Hapiness: {}""")
+        print(f"\n{self.name}'s Status:")
+        print(f" -> Health: {self.health}")
+        print(f" -> Hungry: {self.hungry}")
+        print(f" -> Age: {self.age}")
+        print(f" -> Hapiness: {self.hungry + self.health}")
+
+    def showHapiness(self):
+        hapiness = self.hungry + self.health 
+        if (hapiness < 100):
+            print(f"Your level of hapiness is {hapiness}.")
+            print(f"Make it more happy!")
+        elif (hapiness < 20):
+            print(f"Your hapiness level is {hapiness}.")
+            print(f"It is very sad :(")
+        elif (hapiness >= 100):
+            print(f"Your hapiness level is {hapiness}.")
+            print(f"It is really happy :)")
+        
+
+pet = VirtualPet("Coisa", 80, 50, 2)
+pet.showStatus()
+pet.showHapiness()
+pet.feed()
+pet.showStatus()
